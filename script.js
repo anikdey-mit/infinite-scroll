286a178d9ea6d884ed7f8a101f14f7e7c458b9c4
Anik <anik11ads@gmail.com>
2020-08-25 14:28:21 +1000

    updated js file

--- script.js
+++ script.js
@@ -2,17 +2,35 @@ var imageContainer = document.getElementById('image-container')
 var loader = document.getElementById('loader')
 var photoArray = []
 
+function setAttributes(element,attributes){
+    for (var key in attributes){
+        element.setAttribute(key,attributes[key])
+    }
+}
+
 function displayPhotos(){
     photoArray.forEach((photo) => {
         //vreate <a> to link to unsplash
         var item = document.createElement('a')
-        item.setAttribute('href',photo.links.html)
-        item.setAttribute('target','_blank')
+        // item.setAttribute('href',photo.links.html)
+        // item.setAttribute('target','_blank')
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank'
+        })
         //create <img> for photo
         var img = document.createElement('img')
-        img.setAttribute('src',photo.urls.regular)
-        img.setAttribute('alt',photo.alt_description)
-        img.setAttribute('title',photo.alt_description)
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        })
+        // img.setAttribute('src',photo.urls.regular)
+        // img.setAttribute('alt',photo.alt_description)
+        // img.setAttribute('title',photo.alt_description)
+        //put <img> inside <a>, then put both inside imageContainer element
+        item.appendChild(img)
+        imageContainer.appendChild(item)
     })
 }
 
@@ -26,7 +44,8 @@ async function getPhotos(){
     try{
         var response = await fetch(apiUrl)
         photoArray = await response.json()
-        console.log(photoArray)
+        //console.log(photoArray)
+        displayPhotos()
     } catch(error){
 
     }
